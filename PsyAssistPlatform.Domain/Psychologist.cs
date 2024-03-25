d7c0b281663ff2b39bbd47c120b6b0ea6c5f735b
Josh Khrustalev <khrustalevjosh@gmail.com>
2024-03-25 21:03:22 +0300

    PAP-10: Psychologist model

--- PsyAssistPlatform.Domain/Psychologist.cs
+++ PsyAssistPlatform.Domain/Psychologist.cs
@@ -2,5 +2,19 @@
 
 public sealed class Psychologist
 {
-    
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public TimeZoneInfo TimeZone { get; set; }
+
+    public string RequestsInclude { get; set; }
+
+    public string RequestsExclude { get; set; }
+
+    public int UserId { get; set; }
+
+    public bool IsActive { get; set; }
 }
