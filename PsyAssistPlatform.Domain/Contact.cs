8ecf7e34cc475edf08f28af039d381acfeb0a3db
DMITRY-PC\Дмитрий <_andrea_@inbox.ru>
2024-03-27 21:28:01 +0300

    Contact Model

---
+++ PsyAssistPlatform.Domain/Contact.cs
@@ -0,0 +1,16 @@
+﻿namespace PsyAssistPlatform.Domain;
+
+/// <summary>
+/// Контакт
+/// </summary>
+public sealed class Contact
+{
+    public int Id { get; set; }
+
+    public string? Telegram { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+}
+
