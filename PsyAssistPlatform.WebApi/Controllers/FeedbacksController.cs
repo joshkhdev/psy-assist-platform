cb80992badba69a6ea2f06c68bcd06cb808e5416
Dmitry Lomov <51403021+miitrei@users.noreply.github.com>
2024-08-23 10:36:47 +0300

    [PAP-59] Дополнения для микросервиса отзывов

---
+++ PsyAssistPlatform.WebApi/Controllers/FeedbacksController.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using PsyAssistPlatform.Messages;
+using PsyAssistPlatform.WebApi.Models.Feedback;
+
+namespace PsyAssistPlatform.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FeedbacksController : ControllerBase
+{
+    private readonly IRequestClient<FeedbacksMessage> _requestClient;
+
+    public FeedbacksController(IRequestClient<FeedbacksMessage> requestClient)
+    {
+        _requestClient = requestClient;
+    }
+
+    /// <summary>
+    /// Получить список всех отзывов
+    /// </summary>
+    [HttpGet]
+    public async Task<IEnumerable<FeedbackResponse>> GetAllFeedbacksAsync(CancellationToken cancellationToken)
+    {
+        var response = await _requestClient.GetResponse<FeedbacksMessage>(new List<FeedbackResponse>(), cancellationToken);
+
+        return response.Message.Items;
+    }
+}
---
+++ PsyAssistPlatform.WebApi/Models/Feedback/FeedbackResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace PsyAssistPlatform.WebApi.Models.Feedback;
+
+public record FeedbackResponse
+{
+    public int Id { get; set; }
+
+    public required string Telegram { get; set; }
+
+    public required DateTime? FeedbackDate { get; set; }
+
+    public required string FeedbackText { get; set; }
+}
